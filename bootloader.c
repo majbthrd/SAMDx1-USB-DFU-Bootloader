02661fcce27c9a7f6cf34dce44277f3c2f24c47d
Peter Lawrence <majbthrd@gmail.com>
2021-07-30 09:51:55 -0500

    make Crossworks compatible; increment bcdDevice in USB descriptor to make change evident

--- bootloader.c
+++ bootloader.c
@@ -227,7 +227,7 @@ static void USB_Service(void)
 
 #ifdef USE_DBL_TAP
   #define DBL_TAP_MAGIC 0xf02669ef
-  static volatile uint32_t __attribute__((section(".dont_move"))) double_tap;
+  static volatile uint32_t __attribute__((section(".vectors_ram"))) double_tap;
 #endif
 
 void bootloader(void)
