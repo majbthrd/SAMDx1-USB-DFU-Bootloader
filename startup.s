4dcca11337e3c3490273f00505e00836e854aa83
Matt Trescott <mtc@melexis.com>
2024-01-24 19:51:39 -0500

    Optimize initialization code with LDMIA
    Saves 8 bytes, up to 12 bytes with optional extra shortcut

--- startup.s
+++ startup.s
@@ -2,6 +2,7 @@
  * minimal startup for 1kByte USB DFU bootloader for Atmel SAMD11 microcontrollers
  *
  * Copyright (c) 2018, Peter Lawrence
+ * Copyright (c) 2024 Melexis Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -51,37 +52,60 @@ _vectors:
 
 Reset_Handler:
 
+#ifdef USE_RISKY_SHORTCUT
+  /* Address of load_addresses, determined by experimentation because linker
+   * doesn't support symbols for thumb immediates. Although unlikely, it's
+   * possible load_addresses could end up at a different location when
+   * using a different toolchain; hence this shortcut is disabled by default.
+   * When used, it saves 4 bytes. To enable it:
+   *
+   * DEFINES=-DUSE_RISKY_SHORTCUT make
+   *
+   * Then check that the address is actually correct before flashing it to
+   * your device.
+   */
+  movs r7, #0x40
+#else
+  ldr r7, =load_addresses
+#endif
+  ldm r7!, {r0, r1, r2, r3, r4, r5}
+
   /* optimized to assume non-zero sized 'data' section */
-  ldr r0, =__data_end__
-  ldr r1, =__data_start__
-  ldr r2, =__data_load_start__
 copy_loop:
-  ldm r2!, {r3}
-  stm r1!, {r3}
+  ldm r2!, {r7}
+  stm r1!, {r7}
   cmp r1, r0
   bcc copy_loop
 
-  /* optimized to assume non-zero sized 'bss' section */
-  ldr r0, =__bss_end__
-  ldr r1, =__bss_start__
+  /* optimized to assume non-zero sized 'bss' section and that
+   * __bss_start__ equals __data_end__.
+   */
 zero_loop:
   movs r2, #0
   stm r1!, {r2}
-  cmp r1, r0
+  cmp r1, r3
   bcc zero_loop
 
+  /* Only r4-r8 saved by callee */
   bl bootloader
 
   /* if bootloader returns, we proceed to the user app */
 
-  ldr r1, =0x400 /* origin of user app */
-  ldr r0, =0xE000ED08 /* VTOR register */
-  str r1, [r0] /* point VTOR to user app */
-  ldr r0, [r1] /* load stack pointer from user app */
+  str r5, [r4] /* point VTOR to user app */
+  ldr r0, [r5] /* load stack pointer from user app */
   msr msp, r0
   msr psp, r0
-  ldr r0, [r1, #4] /* load reset address from user app */
+  ldr r0, [r5, #4] /* load reset address from user app */
   mov pc, r0
 
 loop:
   b .
+
+  .align 2
+load_addresses:
+  .word __data_end__
+  .word __data_start__
+  .word __data_load_start__
+  .word __bss_end__
+  .word 0xE000ED08 /* VTOR register address */
+  .word 0x400 /* origin of user app */
